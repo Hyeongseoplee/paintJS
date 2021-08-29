b762c6204221e37b4ac23f3b5b23490ae8fb40e7
vennylee <hyungsup42134@gmail.com>
2021-08-29 18:26:53 +0900

    feat: Fill a background by selecting color

--- app.js
+++ app.js
@@ -2,20 +2,22 @@ const canvas = document.querySelector('.paint-board');
 const ctx = canvas.getContext('2d');
 const colors = document.querySelectorAll('.controls__color');
 const rangeControler = document.querySelector('.range');
+const mode = document.querySelector('.fillAndPaintMode');
 
-ctx.strokeStyle = "black";
-ctx.lineWidth = 2.5;
 canvas.width = 400;
 canvas.height = 700;
 
+ctx.fillStyle="black";
+ctx.strokeStyle = "black";
+ctx.lineWidth = 2.5;
 
 let paint = false;
-// let x = 0;
-// let y = 0; 초기값 설정해주는 이유?
+let filling = false;
+let x = 0;
+let y = 0;
 
 function draw (){
     if(canvas.getContext) {
-
         function doPainting(event) {
             const x = event.offsetX;
             const y = event.offsetY;
@@ -37,22 +39,44 @@ function draw (){
             paint = false;
         }
 
+        mode.addEventListener('click', () => {
+            if(filling === true) {
+                filling = false;
+                mode.value = "fill";
+                console.log(filling);
+            } else {
+                filling = true;
+                mode.value = "paint";
+            }
+        })
+
+        function chageColor(event) {
+            let currentColor = event.target.style.backgroundColor;
+            ctx.strokeStyle = currentColor;
+            ctx.fillStyle = currentColor;
+        }
+
+
         rangeControler.addEventListener('change', () => {
             let currentRange = rangeControler.value;
             ctx.lineWidth = currentRange;
         })
 
         colors.forEach((color) => {
-            let currentColor = color.style.backgroundColor;
-            color.addEventListener('click', () => {
-                ctx.strokeStyle = currentColor;
-            })
+            color.addEventListener('click', chageColor);
         })
+
+        function handleFillColor() {
+            if(filling){
+                ctx.fillRect(0, 0, canvas.width, canvas.height);
+            }
+        }
 
         canvas.addEventListener("mousemove", doPainting);
         canvas.addEventListener("mousedown", startPainting);
-        canvas.addEventListener("mouseleave", stopPainting)
+        canvas.addEventListener("mouseleave", stopPainting);
         canvas.addEventListener("mouseup", stopPainting);
+        canvas.addEventListener("click", handleFillColor);
     } else {
         console.log("canvas-unsupported code here.")
     }
